14559daa6c04be41b80284ac26b1afae8299f560
Rakesh Reddy Yakakti <yrakesh2109@gmail.com>
2025-12-24 05:46:59 +0100

    Clean up: Remove deleted services (zeek, suricata, loki, promtail, traffic-monitor) and update README

--- suricata/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Capture only on wlan0 (hotspot) for IDS data collection
-echo "Starting Suricata to capture on wlan0 (hotspot network) for IDS"
-
-# Execute suricata on wlan0 only
-exec suricata -c /etc/suricata/suricata.yaml -i wlan0 --init-errors-fatal
-
